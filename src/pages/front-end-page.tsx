6d30bab1e93415fb27c00a51c817f4869098c0ec
camelys624 <camel_yangz@163.com>
2022-03-30 22:01:08 +0800

    📃 docs(js style): 添加javascript规范

--- src/pages/front-end-page.tsx
+++ src/pages/front-end-page.tsx
@@ -1,47 +1,47 @@
-import React, {Component} from 'react'
-import {graphql} from 'gatsby'
-import {MDXRenderer} from "gatsby-plugin-mdx"
-import TemplatePage, {ContentType, MdxContent} from "../components/template-page"
+import React, { Component } from 'react'
+import { graphql } from 'gatsby'
+import { MDXRenderer } from "gatsby-plugin-mdx"
+import TemplatePage, { ContentType, MdxContent } from "../components/template-page"
 
 class FrontEndPage extends Component<Element, any> {
-  render() {
-    let result: ContentType[] = []
-    let typeMap: any = {}
-    let readme: MdxContent = {
-      title: '',
-      slug: '',
-      body: ''
-    }
+	render() {
+		let result: ContentType[] = []
+		let typeMap: any = {}
+		let readme: MdxContent = {
+			title: '',
+			slug: '',
+			body: ''
+		}
 
-    // @ts-ignore
-    const {allMdx} = this.props.data
-    allMdx.nodes.forEach((item: any) => {
-      let type:string = item.frontmatter.type
-      let formatItem: MdxContent = {
-        title: item.frontmatter.title,
-        slug: item.slug
-      }
+		// @ts-ignore
+		const { allMdx } = this.props.data
+		allMdx.nodes.forEach((item: any) => {
+			let type: string = item.frontmatter.type
+			let formatItem: MdxContent = {
+				title: item.frontmatter.title,
+				slug: item.slug
+			}
 
-      if (!type) {
-        readme = item
-        return
-      }
+			if (!type) {
+				readme = item
+				return
+			}
 
-      if (!typeMap[type]) {
-        typeMap[type] = {
-          title: type,
-          list: [formatItem]
-        }
-        result.push(typeMap[type])
-      } else {
-        typeMap[type].list.push(formatItem)
-      }
-    })
+			if (!typeMap[type]) {
+				typeMap[type] = {
+					title: type,
+					list: [formatItem]
+				}
+				result.push(typeMap[type])
+			} else {
+				typeMap[type].list.push(formatItem)
+			}
+		})
 
-    return <TemplatePage data={result}>
-      <MDXRenderer>{readme.body || ''}</MDXRenderer>
-    </TemplatePage>
-  }
+		return <TemplatePage data={result}>
+			<MDXRenderer>{readme.body || ''}</MDXRenderer>
+		</TemplatePage>
+	}
 }
 
 export const query = graphql`
--- src/pages/{mdx.slug}.tsx
+++ src/pages/{mdx.slug}.tsx
@@ -1,20 +1,20 @@
-import React, {useEffect} from "react"
-import {graphql} from "gatsby"
-import {MDXRenderer} from "gatsby-plugin-mdx"
+import React, { useEffect } from "react"
+import { graphql } from "gatsby"
+import { MDXRenderer } from "gatsby-plugin-mdx"
 import Layout from "../components/Layout"
-import {initHighlight} from "../utils"
+import { initHighlight } from "../utils"
 
-const BlogPost = ({data}: any) => {
-    useEffect(() => {
-        initHighlight()
-    })
+const BlogPost = ({ data }: any) => {
+	useEffect(() => {
+		initHighlight()
+	})
 
-    return (
-        <Layout>
-            <h2>{data.mdx.frontmatter.title}</h2>
-            <MDXRenderer>{data.mdx.body}</MDXRenderer>
-        </Layout>
-    )
+	return (
+		<Layout>
+			<h2>{data.mdx.frontmatter.title}</h2>
+			<MDXRenderer>{data.mdx.body}</MDXRenderer>
+		</Layout>
+	)
 }
 
 export const query = graphql`
