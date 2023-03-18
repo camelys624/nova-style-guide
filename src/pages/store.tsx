58357491e45324abf742a267afc507191f9ef44a
camelys624 <camel_yangz@163.com>
2023-03-18 18:29:55 +0800

    fix

--- src/pages/store.tsx
+++ src/pages/store.tsx
@@ -5,3 +5,5 @@ class Store extends Component {
     return 'dmxlc3M6Ly8xN2I5YmJkZi0xNmJlLTRjZjMtODU3ZS1iZjMzZDY2YTdhNjZAd3d3LmNhbWVseXYyenouc3RvcmU6MzI0MTI/ZW5jcnlwdGlvbj1ub25lJnNlY3VyaXR5PXRscyZ0eXBlPXdzJnBhdGg9JTJGYWQ2YzI1Y2M1MGIlMkYjenp6'
   }
 }
+
+export default Store
