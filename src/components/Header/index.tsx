7a0040db748178f9a26848ec446b6e459ae33b5f
camelys624 <camel_yangz@163.com>
2023-03-18 18:48:59 +0800

    add store.html

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -11,7 +11,7 @@ const Header = () => {
                 <nav>
                     <ul className={headerStyle.menu}>
                       <li><Link to='/front-end-page'>Front End</Link></li>
-                        <li><Link to='/store'>Back End</Link></li>
+                        <li><a href="/store.html">Back End</a></li>
                         <li>UI/UX Design</li>
                         <li><Link to='/blog-page'>Blog</Link></li>
                         <li>
